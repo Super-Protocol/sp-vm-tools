cab4cf59b43b02af351549bee4a9edb0907d01f7
Petr Evstifeev <petr.evstifeev@superprotocol.com>
2024-09-23 20:01:06 +0000

    real build

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 IMAGE_NAME=tdx-builder
+scripts_dir="$( cd "$( dirname "$0" )" && pwd )"
 
 if [[ "$(docker images -q ${IMAGE_NAME} 2> /dev/null)" == "" || "${FORCE_REBUILD_CONTAINER}" == "1" ]]; then
   echo "Docker image ${IMAGE_NAME} not found or FORCE_REBUILD_CONTAINER is set. Building..."
-  sudo rm -rf ./build
+  sudo rm -rf ${scripts_dir}/build
   docker build -t ${IMAGE_NAME} .
 else
   echo "Docker image ${IMAGE_NAME} already exists."
 fi
 
-scripts_dir="$( cd "$( dirname "$0" )" && pwd )"
 mkdir -p build
 docker run -it --rm -v ${scripts_dir}:/builder --entrypoint /bin/bash ${IMAGE_NAME} -c "source /builder/scripts/build.sh && build_main"
