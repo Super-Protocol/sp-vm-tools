12687013223036ee705f5155bac39df267ec8ed7
Andrey Pogoreltsev <andrey.pogoreltsev@gmail.com>
2024-12-04 19:05:52 +0200

    Update start_super_protocol.sh

--- scripts/start_super_protocol.sh
+++ scripts/start_super_protocol.sh
@@ -259,7 +259,7 @@ check_packages() {
 
     # Check for uplink binary
     if ! command -v uplink &> /dev/null; then
-        echo "Error: uplink is not installed. Installing..."
+        echo "Uplink is not installed. Installing..."
         curl -L https://github.com/storj/storj/releases/latest/download/uplink_linux_amd64.zip -o uplink_linux_amd64.zip
         unzip -o uplink_linux_amd64.zip
         sudo install uplink /usr/local/bin/uplink
