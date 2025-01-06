2c37275f4436ba3616a3a45744afe84a22a37f40
Dmitry <dmitryname@gmail.com>
2025-01-06 21:32:07 +0700

    set default value

--- scripts/start_super_protocol.sh
+++ scripts/start_super_protocol.sh
@@ -22,7 +22,7 @@ DEFAULT_MAC_SUFFIX="56"
 QEMU_PATH=""
 DEFAULT_DEBUG=false
 DEFAULT_ARGO_BRANCH="main"
-DEFAULT_ARGO_PATH="main"
+DEFAULT_ARGO_PATH="default"
 
 TDX_SUPPORT=$(lscpu | grep -i tdx)
 SEV_SUPPORT=$(lscpu | grep -i sev)
