90280b95f1619fb07557eae1ac671464e71eb67b
Vladyslav Marchuk <134324829+marchuk-vlad@users.noreply.github.com>
2025-07-31 12:52:39 +0300

    Removed ca-initializer (#21)

--- ca-initializer/linux_builder/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -a
-docker build -f ./Dockerfile --tag temp-linux_builder ..
-id=$(docker create temp-linux_builder)
-mkdir -p ../dist
-docker cp $id:/app/dist/ca-initializer ../dist/ca-initializer-linux
-docker rm $id
